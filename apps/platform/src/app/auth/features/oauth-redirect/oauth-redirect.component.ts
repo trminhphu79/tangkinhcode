58641976eaf9ffc3e3c2a808b77d1576235500f9
michael-tran <trminhphu79@gmail.com>
2024-12-30 15:55:59 +0700

    remove log

--- apps/platform/src/app/auth/features/oauth-redirect/oauth-redirect.component.ts
+++ apps/platform/src/app/auth/features/oauth-redirect/oauth-redirect.component.ts
@@ -18,16 +18,6 @@ export class OauthRedirectComponent {
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((data: any) => {
       const url = 'http://localhost:3000/api/auth/oauth';
-
-      // const url = 'https://github.com/login/oauth/access_token';
-      // const params = {
-      //   client_id: 'Ov23lis4b1cYWl1kueJv',
-      //   client_secret: '743237bfa800c6d47e64ab62f31f5210733a63b9',
-      //   code: data?.code,
-      //   accept: 'json',
-      //   redirect_uri: 'http://localhost:4200/auth'
-      // };
-
       this.oauthService
         .signIn(url, { code: data.code, credentialType: 'GITHUB' })
         .subscribe((response) => {
